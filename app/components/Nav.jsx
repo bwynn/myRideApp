b3142110fd2ed25f217c1d8ab9cd70836d838cba
Brian Wynn <brian.wynn83@gmail.com>
2017-02-04 22:08:02 -0800

    added menu toggle functionality.

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -2,16 +2,39 @@ const React = require('react');
 const { Link, IndexLink } = require('react-router');
 
 const Nav = React.createClass({
+  getInitialState: function () {
+    return {
+      showMenu: false
+    };
+  },
+
+  toggleShowMenu: function () {
+    let showMenu = this.state.showMenu;
+    this.setState({
+      showMenu: !showMenu
+    });
+  },
+
   // toggle nav drop down
   render: function () {
+    let { showMenu } = this.state;
+    let handleMenu = showMenu ? 'menu-container' : 'menu-container closed';
+
     return (
-      <div>
-        <ul className="menu">
-          <li><IndexLink to="/" activeClassName="active">Login</IndexLink></li>
-          <li><Link to="/profile" activeClassName="active">Profile</Link></li>
-          <li><Link to="/users" activeClassName="active">Users</Link></li>
-          <li><Link to="/logout" activeClassName="active">Log Out</Link></li>
-        </ul>
+      <div className="menu-component">
+        <header className="menu">
+          <div className="menu-toggle-button">
+            <button className="button" onClick={ this.toggleShowMenu }>Toggle Menu</button>
+          </div>
+        </header>
+        <div className={ handleMenu }>
+          <ul className="menu">
+            <li><IndexLink to="/" activeClassName="active" onClick={ this.toggleShowMenu }>Login</IndexLink></li>
+            <li><Link to="/profile" activeClassName="active" onClick={ this.toggleShowMenu }>Profile</Link></li>
+            <li><Link to="/users" activeClassName="active" onClick={ this.toggleShowMenu }>Users</Link></li>
+            <li><Link to="/logout" activeClassName="active" onClick={ this.toggleShowMenu }>Log Out</Link></li>
+          </ul>
+        </div>
       </div>
     );
   },
